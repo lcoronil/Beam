acda9051fd5c0079aa9f1245a49591d12d36e92f
Lee Coronilia <shadowwolf246815@gmail.com>
2022-10-09 22:17:21 -0700

    temp title and description

--- docs/main.js
+++ docs/main.js
@@ -1,11 +1,38 @@
-title = "";
+title = "Ghost Hunt";
 
 description = `
+[TAP] to shoot enermies
 `;
 
-characters = [];
+// array of custom sprites 
+characters = [
+    `
+    llllll
+    ll l l
+    ll l l
+    llllll
+     l  l
+     l  l
+      `,
+];
 
-options = {};
+// Game design variable container 
+const G = {
+    WIDTH: 200,
+    HEIGHT: 200,
+
+    PLAYER_X_POS: 5,
+    PLAYER_Y_POST: 20, 
+};
+
+
+options = {
+    viewSize: {x: G.WIDTH, y: G.HEIGHT},
+    isReplayEnabled: true,
+    isPlayingBgm: true,
+    seed: 1,
+    theme: "dark",
+};
 
 function update() {
 	if (!ticks) {
